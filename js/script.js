90edec9b1d5bbaf5a9385f58cf8b4e56d0aeca9f
Shivam Gupta <shivamguptavns01@gmail.com>
2025-07-01 11:13:08 +0530

    Changing css

--- js/script.js
+++ js/script.js
@@ -49,7 +49,10 @@ document.querySelectorAll('.small-image-3').forEach(images =>{
     }
 });
 
-let countDate = new Date('aug 1, 2021 00:00:00').getTime();
+let countDate = new Date();
+countDate.setDate(countDate.getDate() + 7);
+countDate = countDate.getTime();
+
 
 function countDown(){
 
